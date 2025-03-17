e540ac6854891eed2a3635ae710464bccd808354
brian <brian@erl-brian>
2025-03-17 12:20:33 -0700

    fix missing argument for predict_gradient

--- python/binding/bind_log_edf_gp.cpp
+++ python/binding/bind_log_edf_gp.cpp
@@ -41,17 +41,19 @@ BindLogEdfGaussianProcessImpl(const py::module &m, const char *name) {
             py::arg("invalid_position_var"))
         .def(
             "test",
-            [](const T &self, const Eigen::Ref<const Eigen::MatrixX<Dtype>> &mat_x_test) {
+            [](const T &self, const Eigen::Ref<const Eigen::MatrixX<Dtype>> &mat_x_test, const bool predict_gradient) {
                 Eigen::MatrixX<Dtype> mat_f_out, mat_var_out, mat_cov_out;
                 const long dim = mat_x_test.rows();
                 const long n = mat_x_test.cols();
                 mat_f_out.resize(dim + 1, n);
                 mat_var_out.resize(dim + 1, n);
                 mat_cov_out.resize(dim * (dim + 1) / 2, n);
-                if (!self.Test(mat_x_test, mat_f_out, mat_var_out, mat_cov_out)) { return py::make_tuple(py::none(), py::none(), py::none()); }
+                // TODO - allow varying predict_gradient
+                if (!self.Test(mat_x_test, mat_f_out, mat_var_out, mat_cov_out, predict_gradient)) { return py::make_tuple(py::none(), py::none(), py::none()); }
                 return py::make_tuple(mat_f_out, mat_var_out, mat_cov_out);
             },
-            py::arg("mat_x_test"));
+            py::arg("mat_x_test"),
+            py::arg("predict_gradient") = true);
 }
 
 void
